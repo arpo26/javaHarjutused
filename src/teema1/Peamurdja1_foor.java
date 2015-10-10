408107de1707bed64bb932ea355027db622338b1
Arpo Kööp <akoop@klass1-15-l.intra.itcollege.ee>
2015-10-10 17:22:46 +0300

    java õpe

--- src/teema1/Peamurdja1_foor.java
+++ src/teema1/Peamurdja1_foor.java
@@ -1,5 +1,7 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import lib.Foor;
 
 /**
@@ -17,5 +19,37 @@ import lib.Foor;
  * NB! Kui kirjutad new Foor(); siis IDE pakub sulle Foori importimise võimalust.
  * Kliki punase teksti peale ja vajuta alt+enter.
  */
-public class Peamurdja1_foor {
+public class Peamurdja1_foor extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+     Foor tavaline = new Foor();
+        int arv = 1;
+        while(arv<10) {
+            tavaline.syytaPunane();
+            tavaline.paus(0.5);
+            tavaline.syytaKollane();
+            tavaline.paus(0.5);
+            tavaline.syytaRoheline();
+            tavaline.paus(0.5);
+            tavaline.kustutaRoheline();
+            tavaline.paus(0.5);
+            tavaline.kustutaKollane();
+            tavaline.paus(0.5);
+            tavaline.kustutaPunane();
+            tavaline.paus(0.5);
+            arv=arv+1;
+            System.out.println(arv);
+            for(int i=0; i<10; i=i+1){
+                tavaline.syytaKollane();
+                tavaline.paus(0.5);
+                tavaline.kustutaKollane();
+                tavaline.paus(0.5);
+
+            }
+        }
+
+
+
+    }
 }

910430effbd3763ca94c8f83ed8f13ddff1e3574
Arpo Kööp <akoop@klass1-15-l.intra.itcollege.ee>
2015-10-11 16:32:17 +0300

    laevade grid

--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -16,4 +16,81 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
+    public static void main(String[] args) {
+        int [][] laud = new int [9][9];
+
+        for (int i = 0; i<9, i++) {
+            for (int j = 0, j<9, j++){
+                laud[i][j]=r();
+            }
+        }
+
+
+
+        int[][] laud = {{randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev()},
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0},
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+                {
+                        0, 1, 1, 0, 0, 1, 0, 0, 1, 0
+                },
+        };
+        Scanner kasutaja = new Scanner(System.in);
+
+
+        while (gameover(laud) == true) {
+            System.out.println(Arrays.toString(laud));
+
+            System.out.println("Sisesta üks number, ühest kümneni");
+            int sisestus1 = kasutaja.nextInt();
+            System.out.println("Sisesta veel üks number, ühest kümneni");
+            int sisestus2 = kasutaja.nextInt();
+
+            if ((laud[sisestus1 - 1][sisestus2 - 1]) == 1) {
+                laud[sisestus1 - 1][sisestus2 - 1] = 2;
+                System.out.println("Said laevale pihta!");
+            } else if ((laud[sisestus1 - 1][sisestus2 - 1]) == 0) {
+                laud[sisestus1 - 1][sisestus2 - 1] = 3;
+                System.out.println("Ei saanud laevale pihta!");
+            } else if ((laud[sisestus1 - 1][sisestus2 - 1]) == 2 || (laud[sisestus1 - 1][sisestus2 - 1]) == 3) {
+                System.out.println("Oled juba proovinud");
+            }
+        }
+    }
+
+
+    public static boolean gameover(int[][] laud) {
+        for (int i = 0; i < laud.length; i++) {
+            for (int j = 0; j < laud.length, j++) {
+                if (laud[i][j] == 1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    public static int randLaev() {
+        return(int) (Math.random()*2);
+    }
 }
